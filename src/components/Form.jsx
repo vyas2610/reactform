2d093fb1ecbbc4d4f3f57792b76ecb777b4a51c1
vyas2610 <deepakvyas261092@gmail.com>
2026-04-17 19:32:08 +0530

    Form UI Created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Form = () => {
   return (
     <div>
-        
+        <p>
+            <input type='text' name='uname' id='uname' />
+        </p>
+
+        <p>
+            <input type='password' name='upass' id='upass' />
+        </p>
+
+        <p>
+            <button type='submit'>Login</button>
+        </p>
     </div>
   )
 }
